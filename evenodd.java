e3a14ff824d9bc392f5072582ed8dab285f16ad8
Ayesha75 <41113759+Ayesha75@users.noreply.github.com>
2018-07-24 17:17:48 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,28 @@
+import java.util.*;
+ class evenodd 
+public static void main (string args[])
+ int i,count1,count2;
+for (i=0;i<=100;i++)
+
+{
+
+if (i%2==0) 
+
+
+{
+system.out.println("even"+i);
+count1++;
+
+}
+
+
+
+else 
+{
+system.out.println("odd"+i);
+count2++;
+}
+
+}
+system.out.println("even numbers are "+count1);
+system.out.println("odd numbers are "+count2);
